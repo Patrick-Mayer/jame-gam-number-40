36b8808485887d60151e35f2343df093642f3910
Patrick-Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2024-05-25 19:13:00 -0400

    somehow got my crappy animation to play

---
+++ Assets/Animate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(96f, 96f, 96f);
+    }
+}
